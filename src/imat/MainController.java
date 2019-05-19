78ef2c3f31d82265a5919a50243eaaca2a243b33
Pautrik <patrikemanuelson@gmail.com>
2019-05-19 19:12:07 +0200

    Corrected shopPane's node type

--- src/imat/MainController.java
+++ src/imat/MainController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.SplitPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
@@ -15,7 +16,7 @@ public class MainController implements Initializable {
 
     @FXML private Button toShopButton;
     @FXML private Button earlierRecieptsButton;
-    @FXML private AnchorPane shopPane;
+    @FXML private SplitPane shopPane;
     @FXML private FlowPane testFlowPane;
 
     @FXML private ImageView testImage;
