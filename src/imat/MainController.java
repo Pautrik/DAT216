f93801e202bc2e0028404cb383dc44c431994e7a
pautrik <>
2019-05-22 17:56:52 +0200

    Able to save and restore user info

--- src/imat/MainController.java
+++ src/imat/MainController.java
@@ -4,13 +4,18 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.SplitPane;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
+import se.chalmers.cse.dat216.project.Customer;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
+import se.chalmers.cse.dat216.project.Product;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
@@ -26,12 +31,18 @@ public class MainController implements Initializable {
     @FXML private AnchorPane startPane;
     @FXML private FlowPane testFlowPane;
 
+    @FXML private TextField firstnameEdit;
+    @FXML private TextField lastnameEdit;
+    @FXML private TextField addressEdit;
+    @FXML private TextField emailEdit;
+
     @FXML private ImageView testImage;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 //        testFlowPane.getChildren().add(new ProductItem(this));
         iMatDataHandler = IMatDataHandler.getInstance();
+        populateUserFields(iMatDataHandler.getCustomer());
     }
 
     public IMatDataHandler getIMatDataHandler() {
@@ -62,4 +73,19 @@ public class MainController implements Initializable {
         System.out.println("test");
     }
 
+    private void populateUserFields(Customer customer) {
+        firstnameEdit.setText(customer.getFirstName());
+        lastnameEdit.setText(customer.getLastName());
+        addressEdit.setText(customer.getAddress());
+        emailEdit.setText(customer.getEmail());
+    }
+
+    @FXML
+    private void saveUserFields() {
+        Customer customer = iMatDataHandler.getCustomer();
+        customer.setFirstName(firstnameEdit.getText());
+        customer.setLastName(lastnameEdit.getText());
+        customer.setAddress(addressEdit.getText());
+        customer.setEmail(emailEdit.getText());
+    }
 }
