d2e3749763b299ab8d73fc4024bf642f507b6181
pautrik <>
2019-05-22 18:04:18 +0200

    Added Java class for the shoppingcart_item.fxml

--- src/imat/ShoppingcartItem.java
+++ src/imat/ShoppingcartItem.java
@@ -1,4 +1,31 @@
 package imat;
 
-public class ShoppingcartItem {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import se.chalmers.cse.dat216.project.Product;
+
+import java.io.IOException;
+
+public class ShoppingcartItem extends AnchorPane {
+
+    @FXML private Text nameShoppingcartItem;
+    @FXML private Text priceShoppingCartItem;
+    @FXML private Text amountShoppingcartItem;
+
+    public ShoppingcartItem(MainController controller, Product product) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("shoppingcart_item.fxml"));
+        fxmlLoader.setController(this);
+
+        nameShoppingcartItem.textProperty().set(product.getName());
+        priceShoppingCartItem.textProperty().set(Double.toString(product.getPrice()));
+
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
 }
